cfa97520e95c1f7107ce448df9cfcd7a44ee6608
藤林翔 <sfujibayashi@d21-011-248.astr.tohoku.ac.jp>
2025-09-05 10:28:07 +0900

    commented out imposing floor for energy

--- src/eos/general/general_hydro.cpp
+++ src/eos/general/general_hydro.cpp
@@ -117,11 +117,11 @@ void EquationOfState::ConservedToPrimitive(
         }
 
         // apply pressure/energy floor, correct total energy
-        u_e = (u_e - ke > energy_floor_) ?  u_e : energy_floor_ + ke;
+        //u_e = (u_e - ke > energy_floor_) ?  u_e : energy_floor_ + ke;
         // MSBC: if ke >> energy_floor_ then u_e - ke may still be zero at this point due
         //       to floating point errors/catastrophic cancellation
         w_p = PresFromRhoEg(u_d, u_e - ke, s_cell);
-
+	
         for (int n=0; n<NSCALARS; ++n) {
           Real& s_n = s(n,k,j,i);
           Real& r_n = r(n,k,j,i);
