7e83d498f4e6d2aa0bc8032fc59a7fc378c78651
Sho Fujibayashi <sho.fujibayashi@gmail.com>
2026-09-10 19:53:52 +0900

    minor change in comment

--- src/pgen/blast_helmholtz.cpp
+++ src/pgen/blast_helmholtz.cpp
@@ -276,7 +276,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
       }
     }
 
-    // Abar
+    // Ytot = 1/Abar
     for (int k=ks; k<=ke; ++k) {
       for (int j=js; j<=je; ++j) {
 	for (int i=is; i<=ie; ++i) {
